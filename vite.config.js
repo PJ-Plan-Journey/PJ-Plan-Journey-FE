530c7ba83002131ebd72004f03e97e664dcd9d4d
kim-museong <antjd0419@gmail.com>
2024-07-23 11:28:37 +0900

    fix : 파일절대경로 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [
-            { find: '@components', replacement: '/src/components' },
-            { find: '@hooks', replacement: '/src/components' },
-            { find: '@pages', replacement: '/src/components' },
-            { find: '@routers', replacement: '/src/components' },
-            { find: '@styles', replacement: '/src/components' },
-            { find: '@zustands', replacement: '/src/components' },
-            { find: '@axios', replacement: '/src/components' },
-            { find: '@', replacement: '/src' },
-        ],
-    },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+      { find: '@hooks', replacement: resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: resolve(__dirname, 'src/pages') },
+      { find: '@routers', replacement: resolve(__dirname, 'src/routers') },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
+      { find: '@zustand', replacement: resolve(__dirname, 'src/zustand') },
+      { find: '@axios', replacement: resolve(__dirname, 'src/axios') },
+      { find: '@', replacement: resolve(__dirname, 'src') },
+    ],
+  },
 });
