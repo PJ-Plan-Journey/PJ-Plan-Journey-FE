083d7b8977a350356aef159ac8e5881aa618c521
kim-museong <antjd0419@gmail.com>
2024-08-10 10:11:47 +0900

    feature : 일정 공유 API 연결

--- src/components/plan/board/BoradList.jsx
+++ src/components/plan/board/BoradList.jsx
@@ -1,47 +1,33 @@
 import BoardItem from '@components/plan/board/BoardItem';
 import * as S from '@styles/plan/board/BoradList.style';
-
-const list = [
-  {
-    planId: 1,
-    nickname: '작성자1',
-    cityname: '서울',
-    title: 'title1',
-    isPublished: true,
-    createdAt: '2024-07-27',
-    publishedAt: '2024-07-27',
-    likeCount: 30,
-    commentCount: 50,
-  },
-  {
-    planId: 2,
-    nickname: '작성자2',
-    cityname: '서울',
-    title: 'title2',
-    isPublished: true,
-    createdAt: '2024-07-27',
-    publishedAt: '2024-07-27',
-    likeCount: 1111,
-    commentCount: 230,
-  },
-  {
-    planId: 3,
-    nickname: '작성자3',
-    cityname: '서울',
-    title: 'title3',
-    isPublished: true,
-    createdAt: '2024-07-27',
-    publishedAt: '2024-07-27',
-    likeCount: 10,
-    commentCount: 30,
-  },
-];
+import { useQuery } from '@tanstack/react-query';
 
 const BoradList = () => {
+  const getPlanlist = async () => {
+    try {
+      const { data } = await api.get('plans');
+
+      return data;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['getPlans'],
+    queryFn: getPlanlist,
+  });
+
+  console.log({ data });
+
+  if (isLoading) {
+    return <div>데이터 로딩 중...</div>;
+  }
+
   return (
     <S.BoradListContainer>
       <ul className="board-list">
-        {list.map((item) => (
+        {data?.map((item) => (
           <BoardItem key={item.planId} item={item} />
         ))}
       </ul>
