e76810e4398142359b62eda0d38c6eb9d41b16ee
kim-museong <antjd0419@gmail.com>
2024-11-18 22:51:26 +0900

    refactor : prop에 type 추가
    공통 버튼 컴포넌트 prop에 type을 추가하였습니다

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,3 +1,4 @@
+import { flex } from '@styles/common/common.style';
 import styled from 'styled-components';
 
 const colors = {
@@ -17,14 +18,16 @@ const colors = {
 
 const ButtonStyle = styled.button`
   cursor: pointer;
-  width: 100%;
+  width: ${({ $type }) => ($type === 'icon' ? 'auto' : '100%')};
   background-color: ${({ $variant }) => colors[$variant].backgroundColor};
   color: ${({ $variant }) => colors[$variant].color};
   border: none;
-  border-radius: 15px;
+  border-radius: ${({ $type }) => ($type === 'icon' ? '50%' : '15px')};
   box-shadow: ${({ $variant }) => colors[$variant].boxShadow};
-  padding: 12px 15px;
+  padding: ${({ $type }) => ($type === 'icon' ? '8px' : '12px 15px')};
   transition: all 0.2s ease;
+  font-size: ${({ $type }) => ($type === 'icon' ? '1.2rem' : 'auto')};
+  ${({ $type }) => $type === 'icon' && flex}
 
   &:hover {
     background-color: ${({ $variant }) => colors[$variant].hover};
@@ -38,11 +41,23 @@ const ButtonStyle = styled.button`
  * @param {React.ReactNode} children - 버튼 내부에 표시할 내용.
  * @param {function} onClick - 버튼 클릭 시 실행할 함수.
  * @param {boolean} disabled - 버튼 비활성화 여부. 기본값은 false입니다.
+ * @param {string} type - 버튼 타입을 설정하는 prop. 'button' 또는 'icon' 중 하나를 선택할 수 있으며, 기본값은 'button'입니다.
  *
  */
-const Button = ({ variant = 'fill', children, onClick, disabled = false }) => {
+const Button = ({
+  variant = 'fill',
+  type = 'button',
+  children,
+  onClick,
+  disabled = false,
+}) => {
   return (
-    <ButtonStyle $variant={variant} onClick={onClick} disabled={disabled}>
+    <ButtonStyle
+      $variant={variant}
+      $type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </ButtonStyle>
   );
