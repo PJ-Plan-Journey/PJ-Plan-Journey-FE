5e8b6dfaae43fa5810409e0968945a40aabcef92
kim-museong <antjd0419@gmail.com>
2024-11-18 23:18:24 +0900

    refactor : step 변경 기능 수정
    기존에 setStep가 아닌 next와 prev를 직접 구현하여서 사용할 수 있도록 하였습니다.

--- src/zustands/plan/useStepStore.js
+++ src/zustands/plan/useStepStore.js
@@ -2,7 +2,8 @@ import { create } from 'zustand';
 
 const useStepStore = create((set) => ({
   step: 1,
-  setStep: (step) => set({ step }),
+  nextStep: () => set((state) => ({ step: state.step + 1 })),
+  prevStep: () => set((state) => ({ step: state.step - 1 })),
 }));
 
 export default useStepStore;
