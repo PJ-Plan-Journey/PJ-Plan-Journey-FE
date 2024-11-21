d94800280011d335f9e7d2ed184d46ac0aae849b
kim-museong <antjd0419@gmail.com>
2024-11-21 23:31:30 +0900

    refactor : prev기능 제거 및 setStep재생성

--- src/zustands/plan/useStepStore.js
+++ src/zustands/plan/useStepStore.js
@@ -2,8 +2,8 @@ import { create } from 'zustand';
 
 const useStepStore = create((set) => ({
   step: 1,
+  setStep: (value) => set(() => ({ step: value })),
   nextStep: () => set((state) => ({ step: state.step + 1 })),
-  prevStep: () => set((state) => ({ step: state.step - 1 })),
 }));
 
 export default useStepStore;
