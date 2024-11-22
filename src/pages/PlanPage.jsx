ce029efee5477becf44c93ab9a9a675c3718673f
kim-museong <antjd0419@gmail.com>
2024-11-22 21:43:07 +0900

    refactor : resize기능 제거

--- src/pages/PlanPage.jsx
+++ src/pages/PlanPage.jsx
@@ -1,61 +1,22 @@
-import KakaoMap from '@components/plan/KakaoMap';
 import useStepStore from '@/zustands/plan/useStepStore';
 import * as S from '@styles/plan/PlanPage.style';
-import { useEffect, useRef, useState } from 'react';
-import SelectedList from '@components/plan/place/SelectedList';
-import { FaGripLinesVertical as WidthSizeIcon } from 'react-icons/fa6';
+import { useEffect } from 'react';
 import TitleForm from '@components/plan/TitleForm';
-import useDateStore from '@zustands/plan/useDateStore';
 import usePlaceStore from '@zustands/plan/usePlaceStore';
 import Step from '@components/plan/Step';
 import DateContainer from '@components/plan/DateContainer';
 import useDate from '@hooks/plan/date/useDate';
-
-const MINWIDTH = 37;
+import PlaceContainer from '@components/plan/PlaceContainer';
 
 const PlanPage = () => {
   const { step, setStep } = useStepStore();
   const { dates, setDates } = useDate();
   const { initList, setDay } = usePlaceStore();
-  const [width, setWidth] = useState(MINWIDTH);
-  const [isDragging, setIsDragging] = useState(false);
-  const widthRef = useRef(width);
 
-  const handleMouseDown = () => {
-    setIsDragging(true);
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
-
-  const handleMouseMove = (event) => {
-    if (isDragging) {
-      // Ensure the new width is in a reasonable range
-      const newWidth = Math.min(
-        Math.max(MINWIDTH, (event.clientX / window.innerWidth) * 100),
-        100
-      );
-      widthRef.current = newWidth; // Update the ref value
-      setWidth(newWidth); // Trigger re-render with the new width
-    }
-  };
-
-  // 브라우저 새로고침을 눌렀을 때 알림 표시
   const preventClose = (e) => {
     e.preventDefault();
     e.returnValue = '';
   };
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, [isDragging]);
 
   useEffect(() => {
     (() => {
@@ -66,9 +27,6 @@ const PlanPage = () => {
       window.removeEventListener('beforeunload', preventClose);
     };
   }, []);
-
-  // 날짜 변경 컴포넌트는 width 변경이 필요없어서 조건문 설정
-  const resizeContainerStyle = step >= 2 ? { width: `${width}%` } : {};
 
   useEffect(() => {
     return () => {
@@ -82,17 +40,7 @@ const PlanPage = () => {
     <S.PlanPageContainer>
       <Step dates={dates} />
       {step == 1 && <DateContainer dates={dates} setDates={setDates} />}
-      {step >= 2 && (
-        <>
-          <div className="resize-container" style={resizeContainerStyle}>
-            <SelectedList />
-            <div className="width-size-button" onMouseDown={handleMouseDown}>
-              <WidthSizeIcon />
-            </div>
-          </div>
-          <KakaoMap />
-        </>
-      )}
+      {step >= 2 && <PlaceContainer dates={dates} />}
 
       {step >= 3 && <TitleForm />}
     </S.PlanPageContainer>
